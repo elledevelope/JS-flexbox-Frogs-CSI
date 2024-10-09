e9c463d0dce3dd6dcca0442572d6b09be6845f46
elmira <elledevelope@gmail.com>
2024-10-09 09:50:16 +0200

    Added change colors

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,7 @@ let pond = document.getElementById("pond");
 
 frogB.addEventListener("click", function () {
     pond.style.flexDirection = "column";
+    pond.style.alignItems = "flex-start";
 })
 
 frogA.addEventListener("click", function () {
@@ -24,3 +25,34 @@ frogD.addEventListener("click", function () {
     pond.style.flexDirection = "column-reverse";
     pond.style.alignItems = "flex-end";
 })
+
+
+///////////////////////// ----- colors
+frogA.addEventListener("click", function () {
+    frogA.style.backgroundColor = "whitesmoke";
+    frogB.style.backgroundColor = "crimson";
+    frogC.style.backgroundColor = "green";
+    frogD.style.backgroundColor = "purple";
+})
+
+frogB.addEventListener("click", function () {
+    frogA.style.backgroundColor = "crimson";
+    frogB.style.backgroundColor = "crimson";
+    frogC.style.backgroundColor = "crimson";
+    frogD.style.backgroundColor = "crimson";
+})
+
+frogC.addEventListener("click", function () {
+    frogA.style.backgroundColor = "green";
+    frogB.style.backgroundColor = "green";
+    frogC.style.backgroundColor = "green";
+    frogD.style.backgroundColor = "green";
+})
+
+frogD.addEventListener("click", function () {
+    frogA.style.backgroundColor = "purple";
+    frogB.style.backgroundColor = "purple";
+    frogC.style.backgroundColor = "purple";
+    frogD.style.backgroundColor = "purple";
+})
+
